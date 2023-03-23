735d496065a9a6e0c38541362b1bfa2588100a36
Dan Burnley <dan@danops.uk>
2023-03-23 10:21:15 +0000

    Better part 8.1

--- lib/solutions/day_8.rb
+++ lib/solutions/day_8.rb
@@ -27,41 +27,46 @@ class SimpleGrid
   end
 end
 
-input = get_example_input_for_day(day: 8).map { |l| l.chars.map(&:to_i) }
-grid = CoordinateGrid.new(input)
-simple_grid = SimpleGrid.new(input)
-p simple_grid.rows
-p simple_grid.columns
+input = get_input_for_day(day: 8).map { |l| l.chars.map(&:to_i) }
+grid = SimpleGrid.new(input)
+visible = []
 
-count = 0
-
-grid.all_positions.each do |x,y|
-  p "#{x},#{y}"
-  column = grid.column(x)
-  row = grid.row(y)
-  tree_height = grid.get(x,y)
-
-  if x == 0 or y == 0
-    count += 1
-    next
-  end
-
-  if x == grid.size[:x] - 1 or y == grid.size[:y] - 1
-    count += 1
-    next
-  end
-
-  up = column[0..y-1]
-  down = column[y+1..-1]
-  left = row[0..x-1]
-  right = row[x+1..-1]
-  to_check = [left, right, up, down]
-  to_check.each do |check|
-    if check.all? { |c| c < tree_height }
-      count += 1
-      break
+def get_visible(trees)
+  max_height_seen = -1
+  visible_trees = []
+  trees.each do |tree|
+    if tree > max_height_seen
+      visible_trees << true
+      max_height_seen = tree
+    else
+      visible_trees << false
     end
+  end
+  visible_trees
+end
+# Part one
+grid.rows.each.with_index do |row, y|
+  # Left to right
+  get_visible(row).each.with_index do |tree_visible,x|
+    visible << [x,y] if tree_visible
+  end
+
+  # Right to left
+  get_visible(row.reverse).reverse.each.with_index do |tree_visible,x|
+    visible << [x,y] if tree_visible
   end
 end
 
-p count
+grid.columns.each.with_index do |col, x|
+  # Top to bottom
+  get_visible(col).each.with_index do |tree_visible,y|
+    visible << [x,y] if tree_visible
+  end
+
+  # Bottom to top
+  get_visible(col.reverse).reverse.each.with_index do |tree_visible,y|
+    visible << [x,y] if tree_visible
+  end
+end
+
+puts "Part one: #{visible.uniq.length}"
